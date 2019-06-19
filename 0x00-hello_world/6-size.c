80d145d12609550cc28dcbc53372f262cfa5c0cb
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-06-19 18:58:53 -0500

    print size of variabels on c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,14 +6,10 @@
  */
 int main(void)
 {
-printf("Size of char: %i bytes\n", sizeof(char));
-printf("Size of unsigned char: %i bytes\n", sizeof(unsigned char));
-printf("Size of short: %i bytes\n", sizeof(short));
-printf("Size of unsigned short: %i byte\n", sizeof(unsigned short));
-printf("Size of int: %i byte\n", sizeof(int));
-printf("Size of unsigned int: %i byte\n", sizeof(unsigned int));
-printf("Size of long: %i byte\n", sizeof(long));
-printf("Size of unsigned long: %ld byte\n", sizeof(unsigned long));
-
+printf("Size of char: %i byte(s)\n", sizeof(char));
+printf("Size of int: %i byte(s)\n", sizeof(int));
+printf("Size of long int: %i byte(s)\n", sizeof(long int));
+printf("Size of long long int: %i byte(s)\n", sizeof(long long int));
+printf("Size of float: %i byte(s)\n", sizeof(float));
 return (0);
 }
