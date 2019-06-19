7ef1976380da4d08c94f4c137ca71b1735f15d6c
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-06-19 18:16:23 -0500

    Use a printF to print message

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -  Function prints  a string with print f
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("\"with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
