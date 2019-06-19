8dd6ec05b7fadb9c83c98cfed5af181a6b1144d8
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-06-19 18:02:18 -0500

    Print a mesage

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
-#include<studio.h>
+#include <stdio.h>
 /**
  * main -  Function prints  a string
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
