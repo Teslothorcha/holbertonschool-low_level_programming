5a6baed345d867d2e0c8892477091dbdaeaddd0b
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-06-20 16:12:19 -0500

    Print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main - prints Alphabet in lower case
+ *
+ *Return: (0)
+ */
+int main(void)
+{
+int x;
+for (x = 48; x <= 57; x++)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
