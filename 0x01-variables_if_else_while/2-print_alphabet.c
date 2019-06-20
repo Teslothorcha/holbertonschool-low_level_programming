83968f52f7ca07d8728cd96ab051112d8518db5f
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-06-20 13:35:28 -0500

    Put alphabet in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - prints Alphabet in lower case
+ *
+ * Return: 0
+ */
+int main(void)
+{
+for (int x = 97; x <= 122; x++)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
