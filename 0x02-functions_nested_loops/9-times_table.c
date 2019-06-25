b3c859f0f0d26ea07ada0786f314a9d1a5bfc288
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-06-25 18:44:19 -0500

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,29 +6,30 @@
  */
 void times_table(void)
 {
- int a, b, m;
- 
- for(a = 0; a < 10; a++)
- {
- for(b = 0; b < 10; b++)
- {
- m = a * b;
- if(m >= 10)
-   { 
- _putchar(',');
- _putchar(' ');
- _putchar( (m / 10) + '0');
- _putchar( (m % 10) + '0');
- }
- else 
-   {
+int a, b, m;
+for (a = 0; a < 10; a++)
+{
+for (b = 0; b < 10; b++)
+{
+m = a * b;
+if (m >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
+}
+else if (m < 10)
+{
+if (b > 0)
+{
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
+}
 _putchar(m + '0');
-   }
-   }
- 
+}
+}
 _putchar('\n');
 }
 }
