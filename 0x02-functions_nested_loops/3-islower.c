758fa7af43b4de06f87a1e900ecf47bdfb6ee0d7
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-06-25 10:38:34 -0500

    Verify if char is lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,9 @@
 /**
  * _islower - Verifies if letter are lowercase
  *
- * @c - receibes a char to check it
- * Return: 1 if lowercase detecde else return  Nothing
+ * @c:  receibes a char to check it
+ * Return: 1 if lowercase detecde
+ * Return: 0 if lowercase is not detected  Nothing
  */
 int _islower(int c)
 {
