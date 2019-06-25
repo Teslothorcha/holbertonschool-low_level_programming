12518b1e06a4e3f8010fa77303742d39727b9411
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-06-25 17:56:37 -0500

    update on project

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 /**
  * jack_bauer- Prints every minute of the day
  *
- * Return: 0 on success
+ * Return: no return
  */
 void jack_bauer(void)
 {
@@ -15,7 +15,9 @@ for (b = 48; b <= 57; b++)
 for (c = 48; c <= 53; c++)
 {
 for (d = 48; d <= 57; d++)
+{
 if (!(a == 50 && b > 51))
+{
 _putchar(a);
 _putchar(b);
 _putchar(':');
@@ -28,3 +30,4 @@ _putchar('\n');
 }
 }
 }
+
