37864c850653a22937519d874062beba165846f2
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-06-25 10:20:36 -0500

    print then alpkahbets

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _islower - Verifies if letter are lowercase
+ *
+ *
+ * Return: 1 if lowercase detecde else return  Nothing
+ */
+int _islower(int c)
+{
+while (c > 96 && c < 123)
+{
+return (1);
+}
+return (0);
+}
