2abad1bfbe29e2d4a2a64c11a9da759fc8174973
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-06-25 19:32:36 -0500

    updates to project

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * add - Prints add of two numbers
+ *@i: first number to add
+ *@j: second number to add
+ * Return: addition
+ */
+int add(int i, int j)
+{
+
+return (i + j);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include "stdio.h"
+/**
+ * print_to_98- Prints natural numbers till 98
+ *@n: number from which initiates calculation
+ * Return: addition
+ */
+void print_to_98(int n)
+{
+if(n <= 98)
+{
+for( ;n < 98; n++)
+{
+printf("%d, ",n);
+}
+}
+ else
+{
+for( ;n > 98; n--)
+{
+printf("%d, ",n);
+}
+}
+printf("%d\n", n);
+}
+
