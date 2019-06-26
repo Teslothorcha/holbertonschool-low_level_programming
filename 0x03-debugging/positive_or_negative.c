8fd75aec3b893f4c1b644544f8446c8f9c329426
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-06-25 23:52:30 -0500

    debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * positive_or_negative -  Function prints  if random number is +,- or 0
+ *@n: enter input
+ * Return: Always 0 (Success)
+ */
+int  positive_or_negative(int n)
+{
+if (n == 0)
+printf("%d is zero\n", n);
+else
+if (n > 0)
+printf("%d is positive\n", n);
+else
+printf("%d is negative\n", n);
+return (0);
+}
