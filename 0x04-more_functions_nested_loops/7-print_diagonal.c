76663b60c8e56a7236a4112243cc478b2f8978ca
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-06-27 20:17:20 -0500

    projects

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,18 +8,18 @@ void print_diagonal(int n)
 {
 	int i;
 	int x;
-	for(i = 0; i < n; i++)
-	   {
-		   for(x = 0; x < i; x++)
-		   {
-			   _putchar(' ');
-		   }
-		   _putchar(92);
-		   _putchar('\n');
-	   }
+
+	for (i = 0; i < n; i++)
+	{
+		for (x = 0; x < i; x++)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+		_putchar('\n');
+	}
 	if (n < 1)
 	{
 		_putchar('\n');
 	}
-
 }
