9c9a0bacd58f3062a4ed3e659c2bdd56e8069a76
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-07-04 12:34:50 -0500

    Concatenates strings

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,6 @@
+This is the documentation for more pointers and arrays project
+
+It will be a enhancer of pointers and arrays usage
+
+Author
+Juan David Marin Bernal
---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,8 @@
+#ifndef Holberton_h
+#define Holberton_h
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+
+#endif
