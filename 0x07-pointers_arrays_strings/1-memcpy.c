a404bcfdd301de83ff77fc68880868a9a6246e46
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-07-08 18:14:25 -0500

    add more exercises of pointers

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,18 +1,19 @@
 #include "holberton.h"
 /**
- * _memset - fills memory with a constant byte.
+ * _memcpy - fills memory with a constant byte.
  *
- * @s: string
- * @b: constant to be put in string
+ * @dest: string
+ * @src: constant to be put in string
  * @n: amount of times string will be printed
  * Return:  string.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
+
 	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
-	return(dest);
+	return (dest);
 }
