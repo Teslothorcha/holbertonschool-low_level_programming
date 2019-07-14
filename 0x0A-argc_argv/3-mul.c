f67cdfadf64244f799ddf4fbc17eb8777c44857f
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-07-14 17:55:56 +0000

    [Feature] Multiply if two numbers are given as arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints arguments received
+ * @argc: number of command line arguments
+ * @argv: array containing the program command line arguments
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, mult = 1;
+
+	if(argc > 2 && argc < 4)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mult *= atoi(argv[i]);
+		}
+
+		printf("%d\n", mult);
+		return(0);
+	}
+	else if(!(argc > 2 && argc < 4))
+	{
+		printf("Error\n");
+	}
+	return(1);
+}
