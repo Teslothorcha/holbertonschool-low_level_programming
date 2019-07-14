bc92537b6bc35ca6745c4116b8af861e03791837
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-07-14 03:43:54 +0000

    [fix] add header comments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,10 @@
-#include <stdlib.h>
 #include <stdio.h>
+/**
+ * main - prints name
+ * @argc: number of command line arguments
+ * @argv: array containing the program command line arguments
+ * Return: 0.
+ */
 
 int main (int argc __attribute__((unused)), char* argv[] __attribute__((unused)))
 {
