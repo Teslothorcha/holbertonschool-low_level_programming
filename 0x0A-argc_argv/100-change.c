f50a8a261db190240ca924e31ab5c3f2a0229777
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-07-14 23:14:10 +0000

    [feature] Add digits if found

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - adds positive numbers.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	int suma = 0, str, digit;
+
+	for (str = 1; str < argc; str++)
+	{
+		for (digit = 0; argv[str][digit] != '\0'; digit++)
+		{
+			if (!isdigit(argv[str][digit]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		suma += atoi(argv[str]);
+	}
+	printf("%d\n", suma);
+	return (0);
+}
