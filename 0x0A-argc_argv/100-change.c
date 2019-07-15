73f1160e598cc32e5af4cdff6112e520577932ad
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-07-15 02:09:26 +0000

    [fix] Add a break to stop the module array to iterate

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,15 +10,21 @@ int main(int argc, char *argv[])
 {
 	int coin = 0,  str = 1,  val, i;
 	int coinval[5] = {25, 10, 5, 2, 1};
-	if(argc == 2)
+
+	if(atoi(argv[str]) < 0)
+	{
+		printf("0\n");
+		return(0);
+	}
+	if (argc == 2)
 	{
 		val = atoi(argv[str]);
-		while(val > 0)
+		while (val > 0)
 		{
 			i = 0;
-			for(i = 0; i < 5; i++)
+			for (i = 0; i < 5; i++)
 			{
-				if(val % coinval[i] == 0)
+				if (val % coinval[i] == 0)
 				{
 					val -=  coinval[i];
 					coin++;
@@ -27,7 +33,7 @@ int main(int argc, char *argv[])
 			}
 		}
 		printf("%d\n", coin);
-		return(0);
+		return (0);
 	}
 	printf("Error\n");
 	return (1);
