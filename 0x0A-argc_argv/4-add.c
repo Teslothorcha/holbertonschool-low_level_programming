37f390de8719181fd36f72d3e97fb835e3030104
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-07-15 02:54:53 +0000

    [fix] add a return

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,34 +1,32 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 
 /**
- * main - adds positive numbers.
- * @argc: argument count
- * @argv: argument vector
- * Return: 0.
+ * main - add
+ *@argc: number of arguments
+ *@argv: string with arguments
+ * Return: 0 1
  */
 int main(int argc, char *argv[])
 {
-	int suma, i, j;
+	int suma, str, digit;
 
 	suma = 0;
-	if (argc < 2)
+	if (argc > 1)
 	{
-		printf("0\n");
-		return (0);
-	}
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		for (str = 1; str < argc; str++)
 		{
-			if (!isdigit(argv[i][j]))
+			for (digit = 0; argv[str][digit] != '\0'; digit++)
 			{
-				printf("Error\n");
-				return (1);
+				if (!(isdigit(argv[str][digit])))
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
+			suma += atoi(argv[str]);
 		}
-		suma += atoi(argv[i]);
 	}
 	printf("%d\n", suma);
 	return (0);
