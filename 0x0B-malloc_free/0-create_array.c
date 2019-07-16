7cca20fe8d93fc0073feafd969b54924f281f9d3
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-07-16 17:02:59 +0000

    [Feature] Create and populate an array  with a specified character on dynamic memory

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *create_array - crates array an it dynamic memory initialized
+ * @c: character to popupalte the array with
+ * @size: the size of the array to populate
+ *
+ * Return: Nothing.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *v;
+	unsigned int pos;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	v = malloc(sizeof(char) * size);
+	for (pos = 0; pos < size; ++pos)
+	{
+		v[pos] = c;
+	}
+	return (v);
+}
