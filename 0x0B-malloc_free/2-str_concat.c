4462eb67a311250eb6f44fd31d218380e83a0568
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-07-16 20:40:28 +0000

    [Feature] Concatenates two strings into a newly open space

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,11 +14,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		return (NULL);
+		s1 = " ";
 	}
 	if (s2 == NULL)
 	{
-		return (NULL);
+		s2 = " ";
 	}
 	for (len = 0; s1[len] != '\0'; len++)
 	{
