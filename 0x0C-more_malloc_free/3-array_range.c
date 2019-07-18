58d20910e89ebdcccf177f30965cfc16a4ccf6e8
Teslothorcha <juandavidmarinbernal@gamil.com>
2019-07-18 20:33:47 +0000

    [Feature] Creates an range of integers inside an array

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * *_array_range - Creates an array and initializes it with a range on integers
+ * @min: begining of the range
+ * @max: Limit of the range
+ * Return: Array of integers.
+ */
+int *array_range(int min, int max)
+{
+	int *range;
+	int count;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	range = malloc(sizeof(int) * (max - min));
+	if (!range)
+	{
+		return (NULL);
+	}
+	for (count = 0; count < (max - min); count++)
+	{
+		range[count] =  min + count;
+	}
+	return (range);
+}
