2a00766dfc58b9b61ed0b4a09c25708196f7360c
Teslothorcha <juandavidmarinbernal@gamil.com>
2019-07-22 23:17:42 +0000

    [Feature]  Initialize sturctu dog d

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,10 +10,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 {
-	if (d)
+	if (!d)
 	{
-		(*d).name = name;
-		(*d).age = age;
-		(*d).owner = owner;
+		return;
 	}
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
 }
