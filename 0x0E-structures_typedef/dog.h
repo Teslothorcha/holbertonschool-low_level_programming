2964b3181f1182d4e5f12152a73ccea38f823187
Teslothorcha <juandavidmarinbernal@gamil.com>
2019-07-22 20:21:33 +0000

    [Feature]  creates a structure named dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOGO_H
+#define DOGO_H
+/**
+ * struct dog - name, age, owner.
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Description: sets the type for name, age, owner.
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#enidf DOGO_H
