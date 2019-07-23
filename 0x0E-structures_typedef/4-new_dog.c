679612cd405694d415787613f6e9aff43be434ab
Teslothorcha <juandavidmarinbernal@gamil.com>
2019-07-23 02:17:13 +0000

    [Update]  creates a new dog and saves two parameters

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,8 +1,7 @@
 #include <stdlib.h>
 #include "dog.h"
 /**
- * init_dog - Initializes dog structure
- *@d: struct name
+ * new_dog - Creates a new dog structure variable
  *@name: dog's name
  *@age: dog's age
  *@owner: dog's owner
@@ -13,15 +12,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *copy_nd;
 	int lenght_name, lenght_owner, copy_name, copy_owner;
 
-	copy_nd = malloc (sizeof(dog_t));
-	if(!copy_nd)
+	copy_nd = malloc(sizeof(dog_t));
+	if (!copy_nd)
 	{
 		return (NULL);
 	}
 	for (lenght_name = 0; name[lenght_name] != '\0'; lenght_name++)
 		;
-	(*copy_nd).name = malloc (sizeof(char) * (lenght_name + 1));
-	if(!(*copy_nd).name)
+	(*copy_nd).name = malloc(sizeof(char) * (lenght_name + 1));
+	if (!(*copy_nd).name)
 	{
 		free(copy_nd);
 		return (NULL);
@@ -29,8 +28,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	(*copy_nd).age = age;
 	for (lenght_owner = 0; owner[lenght_owner] != '\0'; lenght_owner++)
 		;
-	(*copy_nd).owner = malloc (sizeof(char) * (lenght_owner + 1));
-	if(!(*copy_nd).owner)
+	(*copy_nd).owner = malloc(sizeof(char) * (lenght_owner + 1));
+	if (!(*copy_nd).owner)
 	{
 		free((*copy_nd).name);
 		free(copy_nd);
