2d43c95b3150050d7b18b3ad9929d15cf2de21db
Teslothorcha <juandavidmarinbernal@gamil.com>
2019-07-23 02:34:12 +0000

    [Update]  creates a new dog and saves two parameters

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,26 +12,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *copy_nd;
 	int lenght_name, lenght_owner, copy_name, copy_owner;
 
-	copy_nd = malloc(sizeof(dog_t));
-	if (!copy_nd)
-	{
-		return (NULL);
-	}
-	for (lenght_name = 0; name[lenght_name] != '\0'; lenght_name++)
-		;
-	(*copy_nd).name = malloc(sizeof(char) * (lenght_name + 1));
-	if (!(*copy_nd).name)
+	copy_nd = malloc(sizeof(*copy_nd));
+	if (copy_nd == NULL || !(name) || !(owner))
 	{
 		free(copy_nd);
 		return (NULL);
 	}
-	(*copy_nd).age = age;
-	for (lenght_owner = 0; owner[lenght_owner] != '\0'; lenght_owner++)
+	for (lenght_name = 0; name[lenght_name]; lenght_name++)
 		;
+	for (lenght_owner = 0; owner[lenght_owner]; lenght_owner++)
+		;
+	(*copy_nd).name = malloc(sizeof(char) * (lenght_name + 1));
 	(*copy_nd).owner = malloc(sizeof(char) * (lenght_owner + 1));
-	if (!(*copy_nd).owner)
+	if (!(*copy_nd).owner || !(*copy_nd).name)
 	{
 		free((*copy_nd).name);
+		free((*copy_nd).owner);
 		free(copy_nd);
 		return (NULL);
 	}
@@ -39,9 +35,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		(*copy_nd).name[copy_name] = name[copy_name];
 	}
+	(*copy_nd).name[copy_name] = '\0';
+	(*copy_nd).age = age;
 	for (copy_owner = 0; copy_owner < lenght_owner; copy_owner++)
 	{
 		(*copy_nd).owner[copy_owner] = name[copy_owner];
 	}
+	(*copy_nd).owner[copy_owner] = '\0';
 	return (copy_nd);
 }
