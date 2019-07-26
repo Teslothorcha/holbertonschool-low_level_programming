ec76128be26712d0d8a03b4b7ef59e35e3e821cd
Teslothorcha <juandavidmarinbernal@gamil.com>
2019-07-26 19:31:23 +0000

    [Feature] Will add all the possible arguments entered

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - will add every possible argument given
+ *@n: ammount of possible arguments
+ *Return: result of addition as integer
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list list_num;
+	unsigned int result = 0, counter;
+
+	va_start(list_num, n);
+	for (counter = 0; counter < n; counter++)
+	{
+		result += va_arg(list_num, int);
+	}
+	va_end(list_num);
+	return (result);
+}
