1e58deb8dce21665d457c715cb00530ec3116d5b
Teslothorcha <juandavidmarinbernal@gamil.com>
2019-07-27 21:58:24 +0000

    [Feature] Document all functions

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,20 @@
+#ifndef VARIADIC_H
+#define VARIADIC_H
+
+#include <stdarg.h>
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+int sum_them_all(const unsigned int n, ...);
+void stirn_g(va_list list);
+void intege_r(va_list list);
+void floa_t(va_list list);
+void cha_r(va_list list);
+void print_all(const char * const format, ...);
+
+typedef struct op
+{
+	char form;
+	void (*func)(va_list);
+}operation ;
+
+#endif
