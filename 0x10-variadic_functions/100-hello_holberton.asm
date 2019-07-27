99af78924fee892cd51ff0f9243bb4d3093b3a19
Teslothorcha <juandavidmarinbernal@gamil.com>
2019-07-27 23:59:26 +0000

    [Feature] Prints on assembly code

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,16 +1,15 @@
-section .data
-	    msg db "Hello Holberton",10 ; 10 is the ASCII code for a new line (LF)
+	section .data
+	    msg db "hello, world!", 0x0a ; 0x0a = \n
+	    .size equ $ - msg
 
 	section .text
-	    global _start
+	    global start
+start:
+	        mov rax, 0x2000004
+	        mov rdi, 1
+	        mov rsi, msg
+	        mov rdx, msg.size
+	        syscall
 
-_start:
-	    mov rax, 1
-	    mov rdi, 1
-	    mov rsi, msg
-	    mov rdx, 13
-	    syscall
-
-	    mov rax, 60
-	    mov rdi, 0
-	    syscall
+	        mov eax, 0x2000001
+	        mov rdi, 0
