ec767ee870cbade3287903c9f73a64dcb5f5063a
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-08-02 04:14:32 +0000

    [Feature] adds a node with str on it

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * add_node - list the nodes of a list strcutre
+ *@head: first structure in the list
+ *@str: string to add to new structure
+ *Return: new structure with str on it
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	size_t n;
+	list_t *add;
+
+	n = 0;
+	add = malloc(sizeof(list_t));
+	if (!add)
+		return (NULL);
+	add->str = strdup(str);
+	while (str[n] != '\0')
+	{
+		n++;
+	}
+		add->next = *head;
+		*head = add;
+	return (*head);
+}
