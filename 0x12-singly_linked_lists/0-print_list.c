443d0879a6a0d9ca95fba192fbc683f85aba1be5
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-08-02 03:05:16 +0000

    [Feature] Print node of a list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * print_list - print the a list strcutre
+ *@h: list to print
+ *Return: number of elemnts
+ */
+size_t print_list(const list_t *h)
+{
+	size_t n;
+	char *ni = "(nil)";
+
+	n = 0;
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%i] %s\n", h->len, ni);
+		}
+		else
+		{
+		printf("[%i] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		n++;
+	}
+	return(n);
+}
