435c21a0660a050d0b287733dc85cd31c94a645a
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-08-04 22:21:18 +0000

    [Feature] will free all space allocated for the list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * add_node_end - add  nodes at the end of the list
+ *@head: first structure in the list
+ *@str: string to add to new structure
+ *Return: new structure with str on it
+ */
+void free_list(list_t *head)
+{
+	list_t *through = NULL;
+
+	while (head)
+	{
+		through = head;
+		head = head->next;
+		fee(through->str);
+		free(through);
+	}
+}
