f1b53a14305143ce250d6d88b3865f040208bc61
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-08-06 16:22:55 +0000

    [Update] added a condition inside the loop

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -8,10 +8,11 @@ int sum_listint(listint_t *head)
 {
 	int sum;
 
+	if (head == NULL)
+		return (0);
+
 	while (head)
 	{
-		if (head == NULL)
-			return (0);
 		sum += head->n;
 		head = head->next;
 	}
