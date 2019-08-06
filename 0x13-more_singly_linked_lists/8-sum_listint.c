1c325d1356568fb8e747b822f554b70e782638f1
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-08-06 16:20:49 +0000

    [Update] added a condition inside the loop

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,20 +1,17 @@
 #include "lists.h"
 /**
- *add_nodeint_end - add node at the end of list
+ *sum_listint - add node at the end of list
  *@head: - pointer to the head of the list
- *@n: component of node
  *Return: pointer to the new  node
  */
 int sum_listint(listint_t *head)
 {
 	int sum;
 
-
-	if (head == NULL)
-		return(0);
-
 	while (head)
 	{
+		if (head == NULL)
+			return (0);
 		sum += head->n;
 		head = head->next;
 	}
