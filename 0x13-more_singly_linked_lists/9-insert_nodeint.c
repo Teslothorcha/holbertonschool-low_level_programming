a7d9ee88c628fc7a8abd0b79883191bc79325442
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-08-06 20:20:05 +0000

    [Feature] Add a node on given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = temp->next;
 		counter++;
 		if (!temp)
-			return(NULL);
+			return (NULL);
 	}
 	new_node->n = n;
 	new_node->next = temp->next;
