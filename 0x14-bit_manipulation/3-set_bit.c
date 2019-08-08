45b791c8507b4d0c4aafbe500a444295a42a3e93
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-08-08 23:46:21 +0000

    [Feature] Add bit at given position

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *set_bit - searches specific bit on number
+ *@n: number to insert bit in
+ *@index: position for bit to be added
+ *Return: 0 if succeded
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index > 63)
+		return (-1);
+	mask = 1 << index;
+	*n |= mask;
+
+	return (0);
+}
