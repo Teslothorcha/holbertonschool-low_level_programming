002f2129d27c36dbf3e7a1bf4b6d564f4bf9c216
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-08-09 01:31:09 +0000

    [Feature] adds a new function to be used

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -8,4 +8,6 @@ int get_bit(unsigned long int n, unsigned int index);
 unsigned int binary_to_uint(const char *b);
 int clear_bit(unsigned long int *n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
 #endif
