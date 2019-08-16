8ad87d4ec3650a6eba64010ba133dd104e611f7b
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-08-16 23:49:17 +0000

    [Fix]added new lines command to error messages

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
 	openfrom = open(argv[1], O_RDONLY);
 	if (openfrom == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	opento = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 00664);
