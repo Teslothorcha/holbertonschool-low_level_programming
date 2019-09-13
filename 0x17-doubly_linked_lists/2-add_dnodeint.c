c8ddc5c4ebdfa78f2c47771dc5df246c124f3a98
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-09-13 16:46:30 +0000

    [Feature] Add a node to adoubly linked list on the head

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * add_dnodeint - add node to  doubly linked list.
+ *@head: head of the double linked list
+ *@n: number to be add on the new node
+ * Return: Always amount of nodes.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+	{
+		return (NULL);
+	}
+
+	if (*head == NULL)
+	{
+		new->n = n;
+		new->next = *head;
+		new->prev = NULL;
+		*head = new;
+	}
+	else
+	{
+		new->n = n;
+		new->next = *head;
+		new->prev = NULL;
+		(*head)->prev = new;
+		*head = new;
+	}
+	return (*head);
+}
