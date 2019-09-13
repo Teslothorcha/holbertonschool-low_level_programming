7f460ba6b050ba87f2eb8fb4f0a5abb8347fa08b
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-09-13 14:49:21 +0000

    [Feature] Print node of a doubly linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_dlistint - print doubly linked list.
+ *@h: head of the double linked list
+ * Return: Always amount of nodes.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("%u\n", (unsigned int) h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
