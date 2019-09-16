74b66cefea210515e18504f7a9d15fabab9b3f92
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-09-16 18:05:21 +0000

    [Feature] change coditional

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,7 +22,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint(h, n));
 	else if (idx == len + 1)
 		return (add_dnodeint_end(h, n));
-	else if (idx > len + 1)
+	else if (idx > len)
 		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
@@ -32,8 +32,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		aux = aux->next;
 		count++;
-		if (aux == NULL)
-			return (NULL);
 	}
 	new->n = n;
 	aux->next->prev = new;
