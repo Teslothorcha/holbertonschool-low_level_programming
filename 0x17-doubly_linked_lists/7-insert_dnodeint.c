76cf387081ec50908f9875104cb9eb9c21efcd40
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-09-16 21:35:49 +0000

    [Fix] check checkers XD

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,42 +1,39 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Function that adds a node at a given position
- * @h: Head of the linked list
- * @idx: Index of new node
- * @n: Number to be added on linked list node
- * Return: The address of the new node or NULL if it failed
+ * insert_dnodeint_at_index - adds node at the beginning of a dlistint_t list.
+ * @h: pointer to the first element of the list
+ * @idx:index
+ * @n:number
+ * Return: the pointer to a new node at the beginning of a dlistint_t list
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new = NULL, *aux = NULL;
-	unsigned int len;
+	dlistint_t *new, *aux, *dlt;
+	unsigned int a = 0;
 
 	aux = *h;
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	for (len = 0; len < idx - 1; len++)
+	while (a < (idx - 1))
 	{
 		aux = aux->next;
+		a++;
 		if (aux == NULL)
 			return (NULL);
 	}
 	if (aux->next == NULL)
 		return (add_dnodeint_end(h, n));
-	if (idx >= len + 2)
+	if (idx >= a + 2)
 		return (NULL);
-
 	new = malloc(sizeof(dlistint_t));
-	if (!new)
+	if (new == NULL)
 		return (NULL);
-
 	new->n = n;
-	new->prev = NULL;
-	new->next = NULL;
-	new->next = aux->next;
-	aux->next->prev = new;
-	aux->next = new;
+	dlt = aux->next;
+	new->next = dlt;
 	new->prev = aux;
-
+	aux->next = new;
+	dlt->prev = new;
 	return (new);
 }
