df4a01fb5a17a4828be11cd231f5e42fc4f3fc1b
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-09-16 19:16:39 +0000

    [Feature] new file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,7 +9,7 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *aux;
+	dlistint_t *new = NULL, *aux = NULL;
 	unsigned int count;
 
 	if (h == NULL)
