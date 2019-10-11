5f0ede1e69fcdc3ff5fd7a6ff643f8d5a57cc9e5
teslothorcha <juandavidmarinbernal@gmail.com>
2019-10-11 16:29:59 +0000

    [Feature] Begining th eagony xd

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash tabel with given amount of index
+ * @size: The size of the hash table
+ *
+ * Return: a pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_hash;
+
+	new_hash =  malloc(sizeof(hash_table_t));
+
+	if (!new_hash)
+		return (NULL);
+
+	new_hash->size = size;
+
+	new_hash->array =  malloc(sizeof(hash_node_t *) * (size));
+	if (!new_hash->array)
+		return (NULL);
+
+	return (new_hash);
+}
