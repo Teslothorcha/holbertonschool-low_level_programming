68da1f2595337e51d1176e30dc4d4154cf32152c
teslothorcha <juandavidmarinbernal@gmail.com>
2019-10-11 22:31:49 +0000

    [Feature] add function that adds keay and value to hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,95 @@
+#include "hash_tables.h"
+/**
+ *hash_table_set - adds an ellement to a hash table
+ *
+ *@ht: hash table to be updated
+ *@key: the key to be hashed
+ *@value: value to be saved and linekd to the key
+ *Return: 1 if succedded
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *tmp, *add_h;
+	unsigned long int ind = 0;
+	char *c_k, *c_v;
+
+	c_k = strdup(key);
+	if (!c_k)
+		return (0);
+	c_v = strdup(value);
+	if (!c_v)
+		return (0);
+	if (!key)
+		return (0);
+	add_h = malloc(sizeof(hash_node_t));
+	if (!add_h)
+		return (0);
+	ind = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[ind];
+	if (tmp == NULL)
+	{
+		add_h->key = malloc(strlen(key) * sizeof(char));
+		if (!add_h->key)
+			return (0);
+		add_h->key = c_k;
+		add_h->value = malloc(strlen(value) * sizeof(char));
+		if (!add_h->value)
+			return (0);
+		add_h->value = c_v;
+		ht->array[ind] = add_h;
+		return (1);
+	}
+	else if (tmp != NULL)
+		return (h_t_s(ht, key, value));
+	return (0);
+}
+/**
+ *h_t_s - adds an ellement to a hash table
+ *
+ *@ht: hash table to be updated
+ *@key: the key to be hashed
+ *@value: value to be saved and linekd to the key
+ *Return: 1 if succedded
+ */
+int h_t_s(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *aux, *add_h;
+	char *c_k, *c_v;
+	unsigned long int ind = 0;
+
+	c_k = strdup(key);
+	if (!c_k)
+		return (0);
+	c_v = strdup(value);
+	if (!c_v)
+		return (0);
+	add_h = malloc(sizeof(hash_node_t));
+	if (!add_h)
+		return (0);
+	ind = key_index((const unsigned char *)key, ht->size);
+	aux =  ht->array[ind];
+	while (aux)
+	{
+		if (strcmp(key, aux->key) == 0)
+		{
+			free(aux->value);
+			aux->value = malloc(strlen(value) * sizeof(char));
+			if (!aux->value)
+				return (0);
+			aux->value = c_v;
+		}
+		aux = aux->next;
+	}
+	aux =  ht->array[ind];
+	add_h->key = malloc(strlen(key) * sizeof(char));
+	if (!add_h->key)
+		return (0);
+	add_h->key = c_k;
+	add_h->value = malloc(strlen(value) * sizeof(char));
+	if (!add_h->value)
+		return (0);
+	add_h->value = c_v;
+	add_h->next = aux;
+	ht->array[ind] = add_h;
+	return (1);
+}
