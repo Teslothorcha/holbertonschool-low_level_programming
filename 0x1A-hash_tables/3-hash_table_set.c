058d6d036f035e19b7b488685018394d326d3567
teslothorcha <juandavidmarinbernal@gmail.com>
2019-10-11 23:58:14 +0000

    [Fix] chaged mallocs

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,15 +11,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *add_h;
 	unsigned long int ind = 0;
-	char *c_k, *c_v;
 
 	if (ht == NULL || key == NULL || value == NULL)
-		return (0);
-	c_k = strdup(key);
-	if (!c_k)
-		return (0);
-	c_v = strdup(value);
-	if (!c_v)
 		return (0);
 	add_h = malloc(sizeof(hash_node_t));
 	if (!add_h)
@@ -27,18 +20,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	ind = key_index((const unsigned char *)key, ht->size);
 	if (ht->array[ind] == NULL)
 	{
-		add_h->key = malloc(strlen(key) * sizeof(char));
+		add_h->key = strdup(key);
 		if (!add_h->key)
 			return (0);
-		add_h->key = c_k;
-		add_h->value = malloc(strlen(value) * sizeof(char));
+		add_h->value = strdup(value);
 		if (!add_h->value)
 			return (0);
-		add_h->value = c_v;
 		add_h->next = NULL;
 		ht->array[ind] = add_h;
 		return (1);
 	}
+	free(add_h);
 	return (h_t_s(ht, key, value));
 }
 /**
@@ -52,15 +44,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 int h_t_s(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *aux, *add_h;
-	char *c_k, *c_v;
 	unsigned long int ind = 0;
 
-	c_k = strdup(key);
-	if (!c_k)
-		return (0);
-	c_v = strdup(value);
-	if (!c_v)
-		return (0);
 	add_h = malloc(sizeof(hash_node_t));
 	if (!add_h)
 		return (0);
@@ -78,8 +63,12 @@ int h_t_s(hash_table_t *ht, const char *key, const char *value)
 		aux = aux->next;
 	}
 	aux = ht->array[ind];
-	add_h->key = c_k;
-	add_h->value = c_v;
+	add_h->key = strdup(key);
+	if (!add_h->key)
+		return (0);
+	add_h->value = strdup(value);
+	if (!add_h->value)
+		return (0);
 	add_h->next = aux;
 	ht->array[ind] = add_h;
 	return (1);
