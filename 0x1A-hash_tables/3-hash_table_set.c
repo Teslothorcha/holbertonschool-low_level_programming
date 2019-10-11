a8ae583ecb0f8ebded279e1573b46d3ed7017a98
teslothorcha <juandavidmarinbernal@gmail.com>
2019-10-11 23:09:16 +0000

    [Fix] if structure is null

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,13 +13,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int ind = 0;
 	char *c_k, *c_v;
 
+	if (ht == NULL || key == NULL || value == NULL)
+		return (0);
 	c_k = strdup(key);
 	if (!c_k)
 		return (0);
 	c_v = strdup(value);
 	if (!c_v)
-		return (0);
-	if (!key)
 		return (0);
 	add_h = malloc(sizeof(hash_node_t));
 	if (!add_h)
