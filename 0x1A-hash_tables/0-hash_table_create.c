037225735372c708235e2b972b8e061c2322e958
teslothorcha <juandavidmarinbernal@gmail.com>
2019-10-12 02:27:28 +0000

    [Feature] free em'all

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,17 +8,18 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash;
+	unsigned int i = 0;
 
 	new_hash =  malloc(sizeof(hash_table_t));
-
 	if (!new_hash)
 		return (NULL);
-
 	new_hash->size = size;
-
 	new_hash->array =  malloc(sizeof(hash_node_t *) * (size));
 	if (!new_hash->array)
 		return (NULL);
-
+	for (i = 0; i < size; i++)
+	{
+		new_hash->array[i] = NULL;
+	}
 	return (new_hash);
 }
