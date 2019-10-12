74e7ac707b313f51b185c017b1b6734c3dafc5e2
teslothorcha <juandavidmarinbernal@gmail.com>
2019-10-12 00:39:08 +0000

    [Fix] added case when array index is empty

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,6 +14,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if  (ht == NULL || key == NULL)
 		return (NULL);
 	ind = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[ind] == NULL)
+		return (NULL);
 	if (ht->array[ind])
 	{
 		tmp = ht->array[ind];
