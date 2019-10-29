db02528c501607646fd8582489d555731ab1aaa6
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-10-29 20:51:31 +0000

    [Fix] added flag to bubble an fixed txt

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -9,17 +9,22 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i = 0, j = 0;
+	int stop = 0;
 
 	for (i = 0; i < size - 1; i++)
 	{
+		stop = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				switch_a(&array[j], &array[j + 1]);
 				print_array(array, size);
+				stop = 1;
 			}
 		}
+		if (stop == 0)
+			break;
 	}
 }
 /**
