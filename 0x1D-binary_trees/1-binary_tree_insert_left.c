bfb4105c18233591193e05c74836182d701c76d5
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-12-04 19:31:45 +0000

    [Fix] parent null

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -9,11 +9,13 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
+	if (!parent)
+		return (NULL);
+
 	new =  malloc(sizeof(binary_tree_t));
 	if (!new)
 		return (NULL);
-	if (!parent)
-		return (NULL);
+
 	new->n = value;
 	new->right = NULL;
 	new->left = NULL;
