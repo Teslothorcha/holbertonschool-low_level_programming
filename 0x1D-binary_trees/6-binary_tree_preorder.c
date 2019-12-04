91884298ab10076166d752a146a6383870afc366
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-12-04 20:37:51 +0000

    [Fix] change contional order

--- 0x1D-binary_trees/6-binary_tree_preorder.c
+++ 0x1D-binary_trees/6-binary_tree_preorder.c
@@ -9,12 +9,11 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 
 
-	if (!tree)
-		return;
+	if (tree)
+	{
+		func(tree->n);
 
-	func(tree->n);
-
-	binary_tree_preorder(tree->left, func);
-	binary_tree_preorder(tree->right, func);
-
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
 }
