e79c3cdd3a85cb033c7f30eb2493d5ac1b9e7ce8
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-12-04 20:32:59 +0000

    [Fix] add func

--- 0x1D-binary_trees/7-binary_tree_inorder.c
+++ 0x1D-binary_trees/7-binary_tree_inorder.c
@@ -7,18 +7,13 @@
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 
-	const binary_tree_t *left = NULL, *right = NULL;
-
 	if (!tree)
 		return;
 
-	if (tree->left)
-		left = tree->left;
-
-	if (tree->right)
-		right = tree->right;
-	binary_tree_inorder(left, &print_num);
-	func(tree->n);
-	binary_tree_inorder(right, &print_num);
-
+	else
+	{
+		binary_tree_inorder(left, &print_num);
+		func(tree->n);
+		binary_tree_inorder(right, &print_num);
+	}
 }
--- 0x1D-binary_trees/8-binary_tree_postorder.c
+++ 0x1D-binary_trees/8-binary_tree_postorder.c
@@ -7,19 +7,14 @@
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 
-	const binary_tree_t *left = NULL, *right = NULL;
 
 	if (!tree)
 		return;
 
-	if (tree->left)
-		left = tree->left;
-
-	if (tree->right)
-		right = tree->right;
-
-	binary_tree_postorder(left, &print_num);
-	binary_tree_postorder(right, &print_num);
-	func(tree->n);
-
+	else
+	{
+		binary_tree_postorder(left, &print_num);
+		binary_tree_postorder(right, &print_num);
+		func(tree->n);
+	}
 }
